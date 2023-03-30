d462408f25381e39626766342f46d4ee1fbb8f5f
CHIAVON Facundo R.     [PROV] <c.fchia@ternium.com.ar>
2023-03-30 18:20:15 -0300

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import { useState } from 'react';
 import styled from 'styled-components';
-import reactLogo from './assets/react.svg';
 import './App.css';
 import Head from './components/Head';
 import Body from './components/Body';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <Head />
-      <Body />          
+      <Body />
     </div>
   );
 }
